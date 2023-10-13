8a55ca0d95ee48d38a563e485551e78c26542486
alcib-22 <ciborowski.alex@gmail.com>
2023-10-12 19:53:51 -0700

    updated project item hover response

--- projects.js
+++ projects.js
@@ -20,65 +20,65 @@ boxOne.addEventListener("mouseout", makeSmall1);
 
 //box 2 ---------
 
-const boxTwo = document.getElementById("box2");
+// const boxTwo = document.getElementById("box2");
 
-const makeBig2 = () => {
-    boxTwo.style.width = '165px';
-    boxTwo.style.height = '120px';
-    boxTwo.innerText = 'Under Construction'
-    boxTwo.style.backgroundColor = '#8C1C13';
+// const makeBig2 = () => {
+//     boxTwo.style.width = '165px';
+//     boxTwo.style.height = '120px';
+//     boxTwo.innerText = 'Under Construction'
+//     boxTwo.style.backgroundColor = '#8C1C13';
 
-}
+// }
 
-const makeSmall2 = () => {
-    boxTwo.style.width = '110px';
-    boxTwo.style.height = '80px';
-    boxTwo.innerText = 'JavaBook'
-    boxTwo.style.backgroundColor = 'lightskyblue';
+// const makeSmall2 = () => {
+//     boxTwo.style.width = '110px';
+//     boxTwo.style.height = '80px';
+//     boxTwo.innerText = 'JavaBook'
+//     boxTwo.style.backgroundColor = 'lightskyblue';
 
-}
+// }
 
-boxTwo.addEventListener('mouseover', makeBig2);
-boxTwo.addEventListener("mouseout", makeSmall2);
-//box 3 ---------
-const box3 = document.getElementById("box3");
+// boxTwo.addEventListener('mouseover', makeBig2);
+// boxTwo.addEventListener("mouseout", makeSmall2);
+// //box 3 ---------
+// const box3 = document.getElementById("box3");
 
-const makeBig3 = () => {
-    box3.style.width = '165px';
-    box3.style.height = '120px';
-    box3.innerText = 'Under Construction'
-    box3.style.backgroundColor = '#8C1C13';
+// const makeBig3 = () => {
+//     box3.style.width = '165px';
+//     box3.style.height = '120px';
+//     box3.innerText = 'Under Construction'
+//     box3.style.backgroundColor = '#8C1C13';
 
-}
+// }
 
-const makeSmall3 = () => {
-    box3.style.width = '110px';
-    box3.style.height = '80px';
-    box3.innerText = 'MyHikes'
-    box3.style.backgroundColor = 'lightskyblue';
+// const makeSmall3 = () => {
+//     box3.style.width = '110px';
+//     box3.style.height = '80px';
+//     box3.innerText = 'MyHikes'
+//     box3.style.backgroundColor = 'lightskyblue';
 
-}
+// }
 
-box3.addEventListener('mouseover', makeBig3);
-box3.addEventListener("mouseout", makeSmall3);
-//box 4 -----------
-const box4 = document.getElementById("box4");
+// box3.addEventListener('mouseover', makeBig3);
+// box3.addEventListener("mouseout", makeSmall3);
+// //box 4 -----------
+// const box4 = document.getElementById("box4");
 
-const makeBig4 = () => {
-    box4.style.width = '165px';
-    box4.style.height = '120px';
-    box4.innerText = 'Under Construction'
-    box4.style.backgroundColor = '#8C1C13';
+// const makeBig4 = () => {
+//     box4.style.width = '165px';
+//     box4.style.height = '120px';
+//     box4.innerText = 'Under Construction'
+//     box4.style.backgroundColor = '#8C1C13';
 
-}
+// }
 
-const makeSmall4 = () => {
-    box4.style.width = '110px';
-    box4.style.height = '80px';
-    box4.innerText = 'Karaoke Sidkekick'
-    box4.style.backgroundColor = 'lightskyblue';
+// const makeSmall4 = () => {
+//     box4.style.width = '110px';
+//     box4.style.height = '80px';
+//     box4.innerText = 'Karaoke Sidkekick'
+//     box4.style.backgroundColor = 'lightskyblue';
 
-}
+// }
 
-box4.addEventListener('mouseover', makeBig4);
-box4.addEventListener("mouseout", makeSmall4);
+// box4.addEventListener('mouseover', makeBig4);
+// box4.addEventListener("mouseout", makeSmall4);
